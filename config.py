63f7626f12c285e50c9fc3c3b5a73dbce4daadc2
shrey2199 <52773351+shrey2199@users.noreply.github.com>
2021-07-17 19:00:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config(object):
-	BOT_TOKEN = ''
-	LD_DOMAIN = '' # Without https:// or http://
-	SECRET = ''
-	ADMIN_IDS = ['',''] # ==> Enter Admin Chat IDs here !!
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+	LD_DOMAIN = os.environ.get("LD_DOMAIN", "") # Without https:// or http://
+	SECRET = os.environ.get("SECRET "")
+	ADMIN_IDS = os.environ.get("ADMIN_IDS", "") # ==> Enter Admin Chat IDs here !!
