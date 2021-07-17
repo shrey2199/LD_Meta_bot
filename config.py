39301f4032603d3ddf75d80b010d357c2a196ee4
shrey2199 <52773351+shrey2199@users.noreply.github.com>
2021-07-17 19:02:36 +0530

    Me Dumb lol

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 	LD_DOMAIN = os.environ.get("LD_DOMAIN", "") # Without https:// or http://
-	SECRET = os.environ.get("SECRET, "")
+	SECRET = os.environ.get("SECRET", "")
 	ADMIN_IDS = os.environ.get("ADMIN_IDS", "") # ==> Enter Admin Chat IDs here !!
