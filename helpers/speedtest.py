766038cba22d53ef73c505af69695a77ae4429ca
shrey2199 <52773351+shrey2199@users.noreply.github.com>
2021-07-31 02:28:30 +0530

    revamping structure

--- helpers/speedtest.py
+++
@@ -1,9 +0,0 @@
-def speed_convert(size):
-    """Hi human, you can't read bytes?"""
-    power = 2 ** 10
-    zero = 0
-    units = {0: "", 1: "Kb/s", 2: "MB/s", 3: "Gb/s", 4: "Tb/s"}
-    while size > power:
-        size /= power
-        zero += 1
-    return f"{round(size, 2)} {units[zero]}"
